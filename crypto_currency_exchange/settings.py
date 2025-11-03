f651394484afea77f7da133604bffd5bcab19614
trlda <lap.dxrii@gmail.com>
2025-11-03 10:01:51 +0200

    init

--- crypto_currency_exchange/settings.py
+++ crypto_currency_exchange/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mrcig06(f^1xlbi0skl5$7^l8_84hxv-5dszvbjre7vvdb5m5$'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api_crypto_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

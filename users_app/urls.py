dbecea2431cbe28d65d8b0a401e787e23315d95b
trlda <lap.dxrii@gmail.com>
2025-11-24 15:31:02 +0200

    added list of all users

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, Users
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path("user/", Users.as_view(), name="users_list"),
     path("register/", RegisterView.as_view(), name="register"),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
